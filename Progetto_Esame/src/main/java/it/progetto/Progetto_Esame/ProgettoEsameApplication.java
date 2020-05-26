c5abf9de844e3ffbefa90d08cfb96fc4612f39ed
Giovanni Totta <S1089860@studenti.univpm.it>
2020-05-26 11:50:12 +0200

    Aggiunta classi GestioneAPI e Utilità

--- Progetto_Esame/src/main/java/it/progetto/Progetto_Esame/ProgettoEsameApplication.java
+++ Progetto_Esame/src/main/java/it/progetto/Progetto_Esame/ProgettoEsameApplication.java
@@ -2,11 +2,13 @@ package it.progetto.Progetto_Esame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import it.progetto.Progetto_Esame.api.*;
 
 @SpringBootApplication
 public class ProgettoEsameApplication {
 
 	public static void main(String[] args) {
+		GestioneAPI.caricamentoJSON("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=covid19&count=3&result_type=popular");
 		SpringApplication.run(ProgettoEsameApplication.class, args);
 	}
 
